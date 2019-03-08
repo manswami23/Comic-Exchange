63eddc6b1d9e9a4f7448fb2bd81702e63c76c22d
Manikandan Swaminathan <mswami2@illinois.edu>
2019-03-08 16:58:46 -0600

    Must change admin to see all users

--- app/__init__.py
+++ app/__init__.py
@@ -47,6 +47,9 @@ def create_app(config_name):
 
     from app import models
 
+    from .admin import admin as admin_blueprint
+    app.register_blueprint(admin_blueprint, url_prefix='/admin')
+
     # import home blueprint
     from .home import home as home_blueprint
     app.register_blueprint(home_blueprint)
