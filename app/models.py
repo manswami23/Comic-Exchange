2bedc0fc4f3668cf90dedc6b9d42953002221f1d
Manikandan Swaminathan <mswami2@illinois.edu>
2019-03-08 19:01:19 -0600

    Added rest of tables

--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ from app import db, login_manager
 
 # Set up a table for many-many relationship between Selling and User
 want_to_sell = db.Table('want_to_sell', db.Column('selling_id', db.Integer, db.ForeignKey('selling.id'), primary_key=True), db.Column('users_id', db.Integer, db.ForeignKey('users.id'), primary_key = True))
-
+hasSold = db.Table('hasSold', db.Column('sold_id', db.Integer, db.ForeignKey('sold.id'), primary_key=True), db.Column('users_id', db.Integer, db.ForeignKey('users.id'), primary_key = True))
 
 class User(UserMixin, db.Model):
     """
@@ -67,7 +67,56 @@ class Selling(db.Model):
     date_posted = db.Column(db.String(10))
     want_to_sell = db.relationship('User', secondary=want_to_sell, lazy='subquery', 
         backref=db.backref('pages', lazy=True))
-
+    book = db.Column(db.Integer, db.ForeignKey('ComicBook.id'),
+        nullable=False)
     def __repr(self):
         return '<Selling: {}>'.format(self.email)
 
+class ComicBook(db.Model):
+    """
+    Create a comic book table
+    """
+
+    __tablename__ = 'ComicBook'
+    id = db.Column(db.Integer, primary_key=True)
+    publisher = db.Column(db.String(50), primary_key=True)
+    series = db.Column(db.String(80), primary_key = True)
+    issueNum = db.Column(db.Integer, primary_key=True)
+    primaryCharacter = db.Column(db.String(50))
+    primaryVillain = db.Column(db.String(50))
+    genre = db.Column(db.String(50))
+    listingsSelling = db.relationship('Selling', backref='comicbook', lazy='dynamic')
+    authoredBy = db.Column(db.Integer, db.ForeignKey('Author.id'), nullable=False)
+    listingsSold = db.relationship('Sold', backref='comicbook', lazy='dynamic')
+    def __repr(self):
+        return '<ComicBook: {}>'.format(self.email)
+
+class Author(db.Model):
+    """
+    Create a author table
+    """
+    
+    __tablename__ = 'Author'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), primary_key=True)
+    works = db.relationship('ComicBook', backref='author', lazy='dynamic')
+
+    def __repr(self):
+        return '<ComicBook: {}>'.format(self.email)
+
+class Sold(db.Model):
+    """
+    Create a table of all sold books
+    """
+
+    __tablename__ = 'sold'
+    id = db.Column(db.Integer, primary_key=True)
+    priceSold = db.Column(db.Float)
+    dateSold = db.Column(db.String(10))
+    hasSold = db.relationship('User', secondary=hasSold, lazy='subquery', 
+        backref=db.backref('pages1', lazy=True))
+    book = db.Column(db.Integer, db.ForeignKey('ComicBook.id'),
+        nullable=False)
+
+    def __repr(self):
+        return '<Sold: {}>'.format(self.email)
