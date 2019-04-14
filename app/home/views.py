d4ecc62dd4fac5a97b7360fc07a76d2be1eda294
Manikandan Swaminathan <mswami2@illinois.edu>
2019-04-14 17:16:20 -0500

    views.py recommended list

--- app/home/views.py
+++ app/home/views.py
@@ -13,21 +13,9 @@ def homepage():
     Render the homepage template on the / route
     
     """
-    engine = db.engine
-    connection = engine.connect()
-    sql = text(
-'SELECT s1.series, s1.issueNum, s1.cgc, s1.price FROM (SELECT comicbook.series, comicbook.primaryCharacter, comicbook.issueNum, selling.cgc, '+
-'selling.price, selling.id as sellID FROM comicbook JOIN selling on comicbook.id = selling.book) as s1, '+
-'(SELECT comicbook.primaryCharacter, COUNT(*) FROM (comicbook JOIN sold ON comicbook.id = sold.book) GROUP BY comicbook.primaryCharacter'+
-' ORDER BY(COUNT(*)) DESC LIMIT 5) as s2' +' WHERE s1.primaryCharacter = s2.primaryCharacter')
-    
-    result = connection.execute(sql).fetchall()
-    for _r in result:
-        print(_r.series + ', #' +str(_r.issueNum)) 
-    connection.close()
 
 
-    return render_template('home/index.html', title="Welcome", output = result)
+    return render_template('home/index.html', title="Welcome")
 
 
 @home.route('/dashboard')
